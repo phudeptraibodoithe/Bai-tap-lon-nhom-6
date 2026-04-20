e6e7b2575f2bb4239187c2ffdc1669523b0229be
ndthai <25020388@vnu.edu.vn>
2026-04-20 23:55:57 +0700

    20/4

--- models/AuctionSession.java
+++ models/AuctionSession.java
@@ -11,17 +11,61 @@ public class AuctionSession {
     private double currentPrice;
     private double bidIncrease; 
     private String status; 
+    private Item item;
+    private StatusOfAuction statusOfAuction;
 
     // Getters
     public int getId() {
         return id;
     }
-    
-    // To be continue
+
+    public StatusOfAuction getStatusOfAuction() {
+        return statusOfAuction;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
 
 
+    // Setters
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
 
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
 
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public void setBidIncrease(double bidIncrease) {
+        this.bidIncrease = bidIncrease;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setStatusOfAuction(StatusOfAuction statusOfAuction) {
+        if (LocalDateTime.now().isBefore(startTime)) {
+            this.statusOfAuction = StatusOfAuction.NOT_STARTED;
+        } else if (LocalDateTime.now().isAfter(endTime)) {
+            this.statusOfAuction = StatusOfAuction.ENDED;
+        } else {
+            this.statusOfAuction = StatusOfAuction.ONGOING;
+        }
+    }
 
     
 }

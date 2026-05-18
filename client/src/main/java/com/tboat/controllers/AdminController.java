d372784ee43c306699698cfd3ac31874f3ff5d1e
25020317 Lương Trần Giang Phú <25020317@vnu.edu.vn>
2026-05-18 20:34:51 +0700

    feat: thêm khả năng đóng gói trong pom

--- client/src/main/java/com/tboat/controllers/AdminController.java
+++ client/src/main/java/com/tboat/controllers/AdminController.java
@@ -1,6 +1,9 @@
 package com.tboat.controllers;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.tboat.models.*;
 import com.tboat.socket.SocketListener;
 import com.tboat.ucb.DataCache;
